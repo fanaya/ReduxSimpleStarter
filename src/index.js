f68710e06549b8342d30c9fa835f51e8b63f4e5e
FANAYA <FANAYA@BLTOMATO.COM>
2018-10-08 18:20:47 -0700

    creating video list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import  React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBDh7OMkJtLczWWu9OkXlgGMaIwNN0NGBc'; // Youtube Data API v3 - browser key
 
 
 
 // create a new component. This component should produce some HTML
-const App = () => {
-    return <div>Hi!</div>; // JSX
+// This is a functional component
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch( {key: API_KEY, term: 'surfboards'}, (videos) => {
+            "use strict";
+            this.setState({ videos });
+        });
+    }
+
+
+    render(){
+
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div> // JSX
+        );
+    }
 };
 
 
