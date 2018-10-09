c99aead1d7d36302281cbfb1b4621862dd7a66e0
FANAYA <FANAYA@BLTOMATO.COM>
2018-10-09 16:37:01 -0700

    finished React YT app

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBDh7OMkJtLczWWu9OkXlgGMaIwNN0NGBc'; // Youtube Data API v3 - browser key
 
 
 
 // create a new component. This component should produce some HTML
-// This is a functional component
 class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [] ,
+            selectedVideo: null
+        };
 
-        YTSearch( {key: API_KEY, term: 'surfboards'}, (videos) => {
-            "use strict";
-            this.setState({ videos });
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch( {key: API_KEY, term: term}, (videos) => {
+            console.log(term);
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
 
     render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div> // JSX
         );
     }
